d07647c76c909ea7856b33a5f830a5d5fc52151c
ThamiresAntunes <antunesthamires8@gmail.com>
2025-06-05 01:42:09 -0300

    feature: adicionando controller do admin

---
+++ backend/src/admin/admin.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { LoginAdminDto } from './dto/login-admin.dto';
+
+@Controller('admin')
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+
+  @Post('login')
+  async login(@Body() dto: LoginAdminDto) {
+    return this.adminService.login(dto);
+  }
+}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { SolicitationModule } from './solicitation/solicitation.module';
 import { AdminService } from './admin/admin.service';
+import { AdminController } from './admin/admin.controller';
 @Module({
   imports: [PrismaModule, SolicitationModule],
   providers: [AdminService],
+  controllers: [AdminController],
 })
 export class AppModule {}
