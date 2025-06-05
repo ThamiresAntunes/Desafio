50eac4efc29473b9cdfb41703f31dbfa2900d820
ThamiresAntunes <antunesthamires8@gmail.com>
2025-06-05 03:01:46 -0300

    fixture: consertando o module do app para autenticacao

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { SolicitationModule } from './solicitation/solicitation.module';
-import { AdminService } from './admin/admin.service';
-import { AdminController } from './admin/admin.controller';
 import { AdminModule } from './admin/admin.module';
 @Module({
   imports: [PrismaModule, SolicitationModule, AdminModule],
-  providers: [AdminService],
-  controllers: [AdminController],
 })
 export class AppModule {}
