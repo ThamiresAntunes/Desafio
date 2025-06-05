f6aae68cebcb30f6a43f108e290e57551edbb87d
ThamiresAntunes <antunesthamires8@gmail.com>
2025-06-05 03:16:37 -0300

    fixture: alterando o module do admin para autenticacao jwt

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -3,15 +3,18 @@ import { JwtModule } from '@nestjs/jwt';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from 'src/jwt/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '2min' },
+      signOptions: { expiresIn: '4h' },
     }),
   ],
   controllers: [AdminController],
-  providers: [AdminService, PrismaService],
+  providers: [AdminService, PrismaService, JwtStrategy],
 })
 export class AdminModule {}
