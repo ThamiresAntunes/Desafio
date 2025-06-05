f5f5645e18c90ae05172291d557aff51f8a5cdd4
ThamiresAntunes <antunesthamires8@gmail.com>
2025-06-05 01:16:16 -0300

    feature: adicionando dto pasta base service admin

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { SolicitationModule } from './solicitation/solicitation.module';
+import { AdminService } from './admin/admin.service';
 @Module({
   imports: [PrismaModule, SolicitationModule],
+  providers: [AdminService],
 })
 export class AppModule {}
