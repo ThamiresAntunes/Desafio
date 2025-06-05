f7b94d72eed9d6fb2ed16b4eecb09140d4f80185
ThamiresAntunes <antunesthamires8@gmail.com>
2025-06-05 01:50:05 -0300

    feature: adicionando module do admin

---
+++ backend/src/admin/admin.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '2min' },
+    }),
+  ],
+  controllers: [AdminController],
+  providers: [AdminService, PrismaService],
+})
+export class AdminModule {}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,9 @@ import { PrismaModule } from './prisma/prisma.module';
 import { SolicitationModule } from './solicitation/solicitation.module';
 import { AdminService } from './admin/admin.service';
 import { AdminController } from './admin/admin.controller';
+import { AdminModule } from './admin/admin.module';
 @Module({
-  imports: [PrismaModule, SolicitationModule],
+  imports: [PrismaModule, SolicitationModule, AdminModule],
   providers: [AdminService],
   controllers: [AdminController],
 })
